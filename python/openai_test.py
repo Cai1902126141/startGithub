8b8fde6cae38f9e9602b5d52cd8195a0444bd7d4
caidongxu <cdx1902126141@outlook.com>
2024-03-17 00:16:54 +0800

    delete openai key

--- python/openai_test.py
+++ python/openai_test.py
@@ -2,8 +2,11 @@
 import os
 import openai
 
-#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
-openai.api_key = "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”
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+print(openai.api_key)
+
+
 
 # 创建一个 GPT-3 请求
 response = openai.Completion.create(
@@ -18,5 +21,4 @@ response = openai.Completion.create(
 
 # 输出 GPT-3 的回答
 print(response.choices[0].text.strip())
-
 

7a63fb9438376cf3c16084577a6e9b3d4113e13a
caidongxu <cdx1902126141@outlook.com>
2024-03-20 00:19:08 +0800

    openai output ssd introduction by HTML

--- python/openai_test.py
+++ python/openai_test.py
@@ -6,7 +6,7 @@ client = OpenAI(
     # This is the default and can be omitted
     api_key=os.environ.get("OPENAI_API_KEY"),
 )
-print(client.api_key)
+//print(client.api_key)
 
 
 def new_completion(user_message,system_message, model="gpt-3.5-turbo"):
@@ -19,7 +19,7 @@ def new_completion(user_message,system_message, model="gpt-3.5-turbo"):
     )
     return chat_completion.choices[0].message.content
     
-system_message = "be a helpful assistant"
-user_message = "Say this is a test."
+system_message = "你是一个专业的健身教练"
+user_message = "我最近在减肥，请问有什么推荐碳水食品供我日常进食"
 response = new_completion(user_message,system_message)
 print(response)

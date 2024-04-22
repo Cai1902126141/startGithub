0afb7899e7253722f7f0e4c0741a61a019bc44d5
caidongxu <cdx1902126141@outlook.com>
2024-04-22 21:45:37 +0800

    updata

--- python/openai_test.py
+++ python/openai_test.py
@@ -6,7 +6,7 @@ client = OpenAI(
     # This is the default and can be omitted
     api_key=os.environ.get("OPENAI_API_KEY"),
 )
-//print(client.api_key)
+print(client.api_key)
 
 
 def new_completion(user_message,system_message, model="gpt-3.5-turbo"):
